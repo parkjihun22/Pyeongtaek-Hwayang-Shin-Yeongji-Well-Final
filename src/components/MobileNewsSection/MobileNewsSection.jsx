4c0007d6a152a8446259df5f045287b58cacb22c
parkjihun22 <eyeful31@naver.com>
2025-12-04 11:23:50 +0900

    feat : 춉춉

--- src/components/MobileNewsSection/MobileNewsSection.jsx
+++ src/components/MobileNewsSection/MobileNewsSection.jsx
@@ -32,7 +32,7 @@ const MobileNewsSection = ({ newsList }) => (
     </div>
 
     <div className={styles.more}>
-      <Link to="/purgio/press">보도자료 더 보기 &gt;</Link>
+      <Link to="/평택화양신영지웰/press">보도자료 더 보기 &gt;</Link>
     </div>
   </section>
 );
